c90bd51a65010cdb32dd744388ba45e6391f4b4b
Степаша <111248475+st3pash@users.noreply.github.com`>
2022-08-27 13:24:08 +0300

    27/08/2022 - 3 tasks from codewars.com

--- 2/task-9.py
+++ 2/task-9.py
@@ -1,9 +1,6 @@
 # regex validate PIN code
 def validate_pin(pin):
-    if pin.isdigit() == False:
-        return False
-    elif len(pin) == 6 or len(pin) == 4:
+    if pin.isdigit() == True and (len(pin) == 6 or len(pin) == 4):
         return True
     else:
         return False
-print(validate_pin(1234.0))
